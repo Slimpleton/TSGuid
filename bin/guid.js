8259e67b70df0819617ae037f81d74345b23795b
Slimpleton <daniel.gione@gmail.com>
2025-11-01 10:00:18 -0700

    added a tsconfig and build folder to make it easier to navigate

--- bin/guid.js
+++ bin/guid.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseGuid = parseGuid;
 exports.parseExactGuid = parseExactGuid;
-var _LENGTH_WITHOUT_DASHES = 32;
-var _CHAR_CHECK = /[0-9a-fA-F]{32}/;
-var _EXACT_GUID_FORMAT = /^[({]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[})]?$/;
+const _LENGTH_WITHOUT_DASHES = 32;
+const _CHAR_CHECK = /[0-9a-fA-F]{32}/;
+const _EXACT_GUID_FORMAT = /^[({]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[})]?$/;
 function isGuid(value) {
     value = value.replaceAll('-', '').trim();
     return value.length == _LENGTH_WITHOUT_DASHES && _CHAR_CHECK.test(value);
