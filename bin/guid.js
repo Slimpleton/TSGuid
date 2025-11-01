e5ab886109eb9a3892aa94e0046a333edec638ad
Slimpleton <daniel.gione@gmail.com>
2025-11-01 12:53:54 -0700

    changed the export fixed logic error

--- bin/guid.js
+++ bin/guid.js
@@ -1,16 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseGuid = parseGuid;
 exports.parseExactGuid = parseExactGuid;
-const _LENGTH_WITHOUT_DASHES = 32;
-const _CHAR_CHECK = /[0-9a-fA-F]{32}/;
-const _EXACT_GUID_FORMAT = /^[({]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[})]?$/;
+exports.parseGuid = parseGuid;
+var _CHAR_CHECK = /^[({]?[0-9a-fA-F]{8}-?([0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12}[})]?$/;
+var _EXACT_GUID_FORMAT = /^[({]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[})]?$/;
 function isGuid(value) {
-    value = value.replaceAll('-', '').trim();
-    return value.length == _LENGTH_WITHOUT_DASHES && _CHAR_CHECK.test(value);
+    return _CHAR_CHECK.test(value);
 }
 function isExactGuid(value) {
-    return !_EXACT_GUID_FORMAT.test(value);
+    return _EXACT_GUID_FORMAT.test(value);
 }
 function parseGuid(value) {
     if (!isGuid(value))
--- guid.ts
+++ guid.ts
@@ -1,4 +1,4 @@
-export type Guid = string & { readonly __brand: unique symbol };
+type Guid = string & { readonly __brand: unique symbol };
 const _CHAR_CHECK: RegExp = /^[({]?[0-9a-fA-F]{8}-?([0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12}[})]?$/;
 const _EXACT_GUID_FORMAT: RegExp = /^[({]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[})]?$/;
 
@@ -10,14 +10,17 @@ function isExactGuid(value: string): value is Guid {
     return _EXACT_GUID_FORMAT.test(value);
 }
 
-export function parseGuid(value: string): Guid {
+function parseGuid(value: string): Guid {
     if (!isGuid(value))
         throw new Error('Invalid guid received');
     return value;
 }
 
-export function parseExactGuid(value: string): Guid {
+function parseExactGuid(value: string): Guid {
     if (!isExactGuid(value))
         throw new Error('Invalid guid received');
     return value;
 }
+
+export type { Guid };
+export { parseExactGuid, parseGuid };
