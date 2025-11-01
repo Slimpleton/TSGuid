d60fb578b95e0234a4d2dd4082c05d9213abc9ff
Slimpleton <daniel.gione@gmail.com>
2025-11-01 13:08:23 -0700

    built the new js variant of the typescript file

--- bin/guid.js
+++ bin/guid.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseExactGuid = parseExactGuid;
 exports.parseGuid = parseGuid;
+exports.createRandomGuid = createRandomGuid;
 var _CHAR_CHECK = /^[({]?[0-9a-fA-F]{8}-?([0-9a-fA-F]{4}-?){3}[0-9a-fA-F]{12}[})]?$/;
 var _EXACT_GUID_FORMAT = /^[({]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[})]?$/;
 function isGuid(value) {
@@ -19,4 +20,7 @@ function parseExactGuid(value) {
     if (!isExactGuid(value))
         throw new Error('Invalid guid received');
     return value;
+}
+function createRandomGuid() {
+    return parseExactGuid(crypto.randomUUID());
 }
